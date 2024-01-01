b29ade98af6c7826d38db53f981df8a0d123edd3
younes-karir <youneskarir@outlook.com>
2024-01-01 17:58:46 +0100

    updates

--- src/Helpers/i18next/Data/Eng.js
+++ src/Helpers/i18next/Data/Eng.js
@@ -12,6 +12,7 @@ export const En = {
     hiThere : "Hi! There",
     status : "I'm a MERN stack developer",
     bio_1 : "Explore my digital realm, where a passion cultivated over the years converges with creativity and practicality, and where innovation seamlessly intertwines with design.",
+    
     // ----- about section -----------
     aboutTitle :"About Me",
     aboutQ_1 : "High Skilled Provide High Quality",
@@ -23,7 +24,76 @@ export const En = {
     emailTitle :"E-Mail",
     adressTitle: "Adress",
     adressData: "Beni Mellal, Morroco.",
- 
+    
+    // ----- resume section -----------
+    resumeTitle: "My Resume",
+    resumeQ_1 : "High Skilled Provide High Quality",
+    resumeExperience :"EXPERIENCE",
+
+      // 1. FULL STACK WEB DEVELOPER  
+      firstJobDate : "01.2023 - NOW",
+      firstJobTitle :"FULL STACK WEB DEVELOPER",
+      firstJobSub : "RirakTECH. | Rabat",
+      firstJobDetails_1 :"Analysis of user needs and creation of technical specifications.",
+      firstJobDetails_2 :"Frontend and backend development (React, Redux, Express, MongoDB).",
+      firstJobDetails_3 :"Integration of tests into the development processes for early error detection and continuous improvement.",
+      firstJobDetails_4 :"Maintenance, enhancement, and addition of new modules.",
+
+      // 2. COMPUTER SCIENCE TEACHER 
+      secoundJobDate : "09.2022 - NOW",
+      secoundJobTitle :"COMPUTER SCIENCE TEACHER",
+      secoundJobSub : "Ministry of National Education. | Béni Mellal",
+      secoundJobDetails_1 :"Teaching various software while adapting to the learners‘ levels.",
+      secoundJobDetails_2 :"Conducting research to assist learners in progressing more quickly.",
+
+    resumeEducation :"EDUCATION",
+
+      // 1. BASIC TRAINING 
+      firstEducationDate : "2022",
+      firstEducationTitle :"BASIC TRAINING",
+      firstEducationSub : "CRMEF | Rabat",
+      firstEducationDetails_1 :"Teacher Qualification Program.",
+
+      // 2. PROFESSIONAL BACHELOR‘S DEGREE 
+      secoundEducationDate : "2020 - 2021",
+      secoundEducationTitle :"PROFESSIONAL BACHELOR‘S DEGREE",
+      secoundEducationSub : "Higher School of Technology | Meknes",
+      secoundEducationDetails_1 :"Specialization in Information and Communication Systems Development.",
+      
+      // 3. UNIVERSITY DIPLOMA OF TECHNOLOGY
+      thirdEducationDate : "2018 - 2020",
+      thirdEducationTitle :"UNIVERSITY DIPLOMA OF TECHNOLOGY",
+      thirdEducationSub : "Higher School of Technology | Meknes",
+      thirdEducationDetails_1 :"Computer Engineering Specialty (Software Engineering Option).",
+  
+    resumeCertification :"CERTIFICATION",
+      
+      // 1.
+      firstCertificateDate : "09.2023",
+      firstCertificateTitle :"Express JS - The Complete Guide.",
+      firstCertificateCredentialId :"UC-83e4dfea-dc8d-4e0a-936f-5f3ddd6a0b94",
+      
+      // 2.
+      secoundCertificateDate : "11.2021",
+      secoundCertificateTitle :"Java Web Services and RESTful API.",
+      secoundCertificateCredentialId :"UC-5d747795-0b3f-493e-93c6-d06579c7b442",
+      
+      // 3.
+      thirdCertificateDate : "11.2021",
+      thirdCertificateTitle :"Web Service REST APIs.",
+      thirdCertificateCredentialId :"UC-a23a3f8e-4c8b-40ca-b136-552888cd8ae8",
+      
+      // 4.
+      fourthCertificateDate : "06.2021",
+      fourthCertificateTitle :"Full-Stack JavaScript Course.",
+      fourthCertificateCredentialId :"UC-0e3dcf5e-fb81-4f2b-9926-1f78735feeb9",
+
+    
+    moreTitle: "More licenses & certifications",
+    skillsTitle: "Skills",
+    toolsTitle : "Tools",
+
+
  
  
   },
